b1e8b2a16bd0c687312b03da4656d9e6a8b6449d
s0nought <70777299+s0nought@users.noreply.github.com>
2023-05-30 01:23:19 +0300

    conftest.py

---
+++ src/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+
+from modules.assertions import assert_status_code
+from modules.constants import BASE_URL_API
+from modules.credentials import credentials
+
+@pytest.fixture(scope = "session", autouse = True)
+def api_session() -> requests.sessions.Session:
+    """Set up an authenticated Session"""
+
+    session = requests.Session()
+
+    url = f"{BASE_URL_API}/Member/Authenticate"
+    data = credentials
+
+    response = session.get(url, json = data)
+
+    assert_status_code(response, 200)
+
+    yield session
