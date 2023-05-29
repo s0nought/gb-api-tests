f7139c95e402dd75c1702953bf9f1bae3c13338c
s0nought <70777299+s0nought@users.noreply.github.com>
2023-05-30 02:28:16 +0300

    Fix method, remove autouse

--- src/conftest.py
+++ src/conftest.py
@@ -5,7 +5,7 @@ from modules.assertions import assert_status_code
 from modules.constants import BASE_URL_API
 from modules.credentials import credentials
 
-@pytest.fixture(scope = "session", autouse = True)
+@pytest.fixture(scope = "session")
 def api_session() -> requests.sessions.Session:
     """Set up an authenticated Session"""
 
@@ -14,7 +14,7 @@ def api_session() -> requests.sessions.Session:
     url = f"{BASE_URL_API}/Member/Authenticate"
     data = credentials
 
-    response = session.get(url, json = data)
+    response = session.post(url, json = data)
 
     assert_status_code(response, 200)
 
