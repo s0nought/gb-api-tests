51a727e7b4220b3a577b3c155b65f0ec5bc7122c
s0nought <70777299+s0nought@users.noreply.github.com>
2023-05-30 22:33:17 +0300

    Remove image constants

--- src/modules/constants.py
+++ src/modules/constants.py
@@ -4,9 +4,6 @@ BASE_URL_API = f"{BASE_URL}/apiv11"
 GAME_ID = 6767
 USER_NAME = "nnmnnms"
 
-EVENT_IMAGE_BASE_URL = "https://images.gamebanana.com/img/banners/events/"
-MOD_IMAGE_BASE_URL = "https://files.gamebanana.com/img/ss/mods/"
-
 LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \
 veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
